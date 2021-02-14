1a8d3834919bd698e899088cc94331282de7519c
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-02-15 01:48:07 +0600

    Add files via upload

---
+++ 1008.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+int main()
+{
+    int a,b;
+    double c, salary;
+    scanf("%d%d%lf",&a,&b,&c);
+
+    salary = (b*c);
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n",a,salary);
+
+
+    return 0;
+}
+
+
+
+
+
